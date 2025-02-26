7dc48907dedb754d35a4c20193014c4c290cf6de
Soanpapdi2517 <152783492+Soanpapdi2517@users.noreply.github.com>
2025-02-26 17:37:53 +0530

    26/02/2025

--- src/Components/Cart/CartSearch.jsx
+++ src/Components/Cart/CartSearch.jsx
@@ -1,8 +1,9 @@
 import { useDispatch } from "react-redux";
 import { addToCart } from "../../Store/Slices/CartSlice";
+import toast from "react-hot-toast";
 
 const CartSearch = ({ items }) => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
   return (
     <div className="flex gap-2 shadow-md rounded-lg p-2 mb-4 items-center ml-4">
       <img src={items.img} className="w-[60px] h-[40px] aspect-square" alt="" />
@@ -11,9 +12,9 @@ const CartSearch = ({ items }) => {
           <h1 className="font-bold text-gray-700">{items.name}</h1>
         </div>
         <div className="flex gap-20 md:gap-1 lg:gap-28">
-        <span className="text-green-500">₹{items.price}</span>
+          <span className="text-green-500">₹{items.price}</span>
           <button
-            onClick={() =>
+            onClick={() => {
               dispatch(
                 addToCart({
                   id: items.id,
@@ -22,9 +23,11 @@ const CartSearch = ({ items }) => {
                   img: items.img,
                   quantity: 1,
                 })
-              )
-            }
-            className="p-1 px-2 text-white bg-green-500 hover:bg-green-600 rounded-lg text-sm"
+              );
+
+              toast.success(`${items.name} Added`);
+            }}
+            className="active:bg-green-600 active:scale-110 active:text-white p-1 px-2 text-white bg-green-500 hover:bg-green-600 rounded-lg text-sm"
           >
             Add To Cart
           </button>
